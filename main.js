1f34cf6bb8b8b28d5614216b2fae7fb95ea73efc
cameronglenn <camglenn8@gmail.com>
2025-11-05 21:27:22 -0500

    Created the isInputFilled() function which determines if a user has entered anything into the task field.

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 // Selectors
 let addTaskBtn = document.getElementById("addTaskBtn")
-let task = document.getElementById("textInput");
+let taskInput = document.getElementById("textInput");
 let taskList = document.getElementById("taskList");
 
+
+// Name         : isInputFilled
+// Description  : This function is used to check if a user has inputted text inside a text input.
+// Parameters   : Void.
+// Return Value : Bool True     :   If user has entered text.
+//                Bool False    :   If user has NOT entered text. 
+function isInputFilled() {
+    // Get the data from the input field.
+    let data = taskInput.value;
+
+    // Remove all whitespace from text & check if empty.
+    data = data.trim();
+    if (data == "") {
+        // Prompt user to enter text.
+        alert("ENTER TEXT");
+
+        return false;
+    }
+
+    return true;
+}
+
+// Event Listener
+addTaskBtn.addEventListener("click", function () {
+
+    // Check to see if anything has been entered into text field. 
+    let result = isInputFilled();
+
+
+});
