006508ed7b6d03a3d3b650c41ca209498cba66ac
cameronglenn <camglenn8@gmail.com>
2025-11-05 21:03:57 -0500

    Added JS selectors.

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-let addTaskBttn = document.getElementById("")
+// Selectors
+let addTaskBtn = document.getElementById("addTaskBtn")
+let task = document.getElementById("textInput");
+let taskList = document.getElementById("taskList");
+
