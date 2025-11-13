f33b83f7facc97fc39df6a00b2658056a5dfcd74
cameronglenn <camglenn8@gmail.com>
2025-11-12 22:54:42 -0500

    Cleaned up the architecture of the application. Removed the the redundant class and added a plain 'tasks.js' file.

--- main.js
+++ main.js
@@ -1,86 +1,9 @@
-import TaskList from "./taskList.js";
+import {addTask, isTaskEmpty} from "./tasks.js"; 
 
 // Selectors
 let addTaskBtn = document.getElementById("addTaskBtn"); 
 let taskInput = document.getElementById("taskInput");
-let errorMsg = document.getElementById("taskErrorMsg"); 
-let listOfTasks = document.getElementById("tasks");  
-
-// Initiate TaskList Object.
-let taskList = new TaskList();
-
-
-// Name         : isInputFilled
-// Description  : This function is used to check if a user has inputted text inside a text input.
-// Parameters   : Void.
-// Return Value : Bool True     :   If user has entered text.
-//                Bool False    :   If user has NOT entered text. 
-function isTaskEmpty() 
-{
-    // Get the data from the input field.
-    let data = taskInput.value;
-
-    // Remove all whitespace from text & check if empty.
-    data = data.trim();
-    if (data == "") 
-    {
-        return "";
-    }
-
-    return data;
-}; 
-
-
-// Name         : deleteTask
-// Description  : This callback function is tied to the delete button event handler. It will remove the specific task from the task list + update 
-//                the task list afterwards.  
-// Parameters   : string task   :   This is the tasks data. 
-// Return Value : Void.  
-function deleteTask(task)
-{
-    // Call the taskList object's method to remove the task from the array of tasks. 
-    taskList.removeTask(task); 
-}
-
-// Name         : updateTaskList
-// Description  : This function is used to update the Task List UI. 
-// Parameters   : taskList[]    :   This is an array of task lists. 
-// Return Value : Void.
-function updateTaskList(tasks) 
-{
-    // Clear the currently displayed tasks. 
-    listOfTasks.innerHTML = "";  
-
-    // Loop through each task & update the taskList UI.
-    tasks.forEach((task, index) => {
-        // Generate a new "li" element.
-        const newTask = document.createElement("li"); 
-
-        // Create a span for the task data & add the task contents.
-        const taskData = document.createElement("span"); 
-        taskData.textContent = task;    
-
-        // Create the delete button for the task & add text.
-        const deleteBtn = document.createElement("button"); 
-        deleteBtn.textContent = "Delete"; 
-
-        // Added an Event Listener for each dynamically created delete button. 
-        deleteBtn.addEventListener("click", () => {
-            // Remove the task. 
-            taskList.removeTask(index);    
-
-            // Update the task list.
-            updateTaskList(taskList.listOfTasks);   
-        }); 
-
-        // Add the textData and the deleteBtn to the LI. 
-        newTask.appendChild(taskData);
-        newTask.appendChild(deleteBtn); 
-
-        // Add the newTask to the listOfTasks UL.
-        listOfTasks.appendChild(newTask); 
-    });
-};
+let errorMsg = document.getElementById("taskErrorMsg");  
 
 // Event Listener for Clicking the Add Task Button. 
 addTaskBtn.addEventListener("click", function () 
@@ -99,13 +22,7 @@ addTaskBtn.addEventListener("click", function ()
     }
 
     // Add the task. 
-    taskList.AddTask(taskEntered);
-
-    // Update the Task List UI. 
-    updateTaskList(taskList.listOfTasks);  
-
-    // Clear the task input field.
-    taskInput.value = "";  
+    addTask(taskEntered); 
 });
 
 // Event listener for pressing ENTER to add a task. 
@@ -117,5 +34,3 @@ taskInput.addEventListener("keydown", function(event)
         addTaskBtn.click(); 
     }
 });
-
-// Event listener for clicking the Remove Task button. 
---
+++ tasks.js
@@ -0,0 +1,86 @@
+// File Name    : tasks.js
+// Purpose      : The purpose of this file is to store all logic for the tasks section of the web application. 
+
+// Selectors
+let taskInput = document.getElementById("taskInput");
+let listOfTasks = document.getElementById("tasks");  
+
+// Initiate TaskList array.
+let taskList = []; 
+
+// Name         : isInputFilled
+// Description  : This function is used to check if a user has inputted text inside a text input.
+// Parameters   : Void.
+// Return Value : Bool True     :   If user has entered text.
+//                Bool False    :   If user has NOT entered text. 
+export function isTaskEmpty() 
+{
+    // Get the data from the input field.
+    let data = taskInput.value;
+
+    // Remove all whitespace from text & check if empty.
+    data = data.trim();
+    if (data == "") 
+    {
+        return "";
+    }
+
+    return data;
+}; 
+
+// Name         : updateTaskList
+// Description  : This function is used to update the Task List UI. 
+// Parameters   : taskList[]    :   This is an array of task lists. 
+// Return Value : Void.
+function updateTaskList(tasks) 
+{
+    // Clear the currently displayed tasks. 
+    listOfTasks.innerHTML = "";  
+
+    // Loop through each task & update the taskList UI.
+    tasks.forEach((task, index) => 
+    {
+        // Generate a new "li" element.
+        const newTask = document.createElement("li"); 
+
+        // Create a span for the task data & add the task contents.
+        const taskData = document.createElement("span"); 
+        taskData.textContent = task;    
+
+        // Create the delete button for the task & add text.
+        const deleteBtn = document.createElement("button"); 
+        deleteBtn.textContent = "Delete"; 
+
+        // Add the textData and the deleteBtn to the LI. 
+        newTask.appendChild(taskData);
+        newTask.appendChild(deleteBtn); 
+
+        // Add the newTask to the listOfTasks UL.
+        listOfTasks.appendChild(newTask); 
+
+        // Added an Event Listener for each dynamically created delete button. 
+        deleteBtn.addEventListener("click", () => {
+            // Remove the task. 
+            taskList.splice(index, 1);    
+
+            // Update the task list.
+            updateTaskList(taskList);    
+        }); 
+    });
+};
+
+// Name         : addTask
+// Description  : This function is used to add a task to the taskList[] & update the UI. 
+// Parameters   : string taskEntered    :   This is the task data that the user input into the text field. 
+// Return Value : Void. 
+export function addTask(taskEntered)
+{
+    // Add the task to the taskList[].
+    taskList.push(taskEntered); 
+
+    // Update the Task List UI. 
+    updateTaskList(taskList);  
+
+    // Clear the task input field.
+    taskInput.value = "";  
+}
